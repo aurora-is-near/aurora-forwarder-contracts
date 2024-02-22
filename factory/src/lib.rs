67f60001f146f99f815de9f5653441f32401106d
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-02-22 17:09:19 +0000

    feat: storage deposit for wnear in factory

--- factory/src/lib.rs
+++ factory/src/lib.rs
@@ -1,12 +1,15 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::serde_json::json;
-use near_sdk::{assert_self, env, near_bindgen, AccountId, Balance, Gas, PanicOnDefault, Promise};
+use near_sdk::{
+    assert_self, env, ext_contract, near_bindgen, AccountId, Balance, Gas, PanicOnDefault, Promise,
+};
 
 const FORWARDER_WASM: &[u8] = include_bytes!("../../res/aurora-forwarder.wasm");
 const INIT_BALANCE: Balance = 1_800_000_000_000_000_000_000_000;
+const STORAGE_BALANCE_BOUND: Balance = 1_250_000_000_000_000_000_000;
 const FORWARDER_NEW_GAS: Gas = Gas(1_000_000_000_000);
-const MAX_NUM_CONTRACTS: usize = 10;
+const MAX_NUM_CONTRACTS: usize = 8;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -41,7 +44,7 @@ impl AuroraForwarderFactory {
         assert!(!parameters.is_empty(), "Parameters can't be empty");
         assert!(
             parameters.len() <= MAX_NUM_CONTRACTS,
-            "Number of parameters can be less or equal 10"
+            "Number of contracts can't be greater than {MAX_NUM_CONTRACTS}"
         );
 
         parameters
@@ -63,7 +66,12 @@ impl AuroraForwarderFactory {
                     .create_account()
                     .transfer(INIT_BALANCE)
                     .deploy_contract(FORWARDER_WASM.to_vec())
-                    .function_call("new".to_string(), args, 0, FORWARDER_NEW_GAS);
+                    .function_call("new".to_string(), args, 0, FORWARDER_NEW_GAS)
+                    .then(
+                        ext_token::ext(params.wnear_contract_id)
+                            .with_attached_deposit(STORAGE_BALANCE_BOUND)
+                            .storage_deposit(forwarder_id.clone()),
+                    );
 
                 forwarder_id
             })
@@ -81,6 +89,11 @@ impl AuroraForwarderFactory {
     pub const fn get_fees_contract_id(&self) -> &AccountId {
         &self.fees_contract_id
     }
+}
+
+#[ext_contract(ext_token)]
+pub trait ExtToken {
+    fn storage_deposit(&self, account_id: AccountId);
 }
 
 #[derive(Deserialize, Serialize)]
--- tests/src/tests/native.rs
+++ tests/src/tests/native.rs
@@ -3,17 +3,19 @@ use crate::sandbox::factory::Factory;
 use crate::sandbox::fungible_token::FungibleToken;
 use crate::sandbox::Sandbox;
 use aurora_forwarder_factory::DeployParameters;
-use near_sdk::serde_json::json;
 use near_workspaces::types::NearToken;
+use near_workspaces::AccountId;
+use once_cell::sync::Lazy;
 
 const BALANCE: NearToken = NearToken::from_near(10);
+static NEAR: Lazy<AccountId> = Lazy::new(|| "near".parse().unwrap());
 
 #[tokio::test]
 async fn test_forward_native_tokens() {
-    let transfer = NearToken::from_near(5);
+    let transfer = NearToken::from_near(1);
     let sandbox = Sandbox::new().await.unwrap();
     let alice = sandbox.create_subaccount("alice", BALANCE).await.unwrap();
-    let (wrap, wrap_owner) = sandbox.deploy_wrap_near().await.unwrap();
+    let (wrap, _) = sandbox.deploy_wrap_near().await.unwrap();
     let fees = sandbox.deploy_fees(&[wrap.id()]).await.unwrap();
     let silo = sandbox.deploy_aurora("silo").await.unwrap();
     let erc20 = silo.deploy_erc20(wrap.id()).await.unwrap();
@@ -33,7 +35,6 @@ async fn test_forward_native_tokens() {
     wrap.storage_deposit(alice.id()).await.unwrap();
     wrap.storage_deposit(fees.id()).await.unwrap();
     wrap.storage_deposit(silo.id()).await.unwrap();
-    wrap.storage_deposit(&forwarder).await.unwrap();
 
     let result = alice.transfer_near(&forwarder, transfer).await.unwrap();
     assert!(result.is_success());
@@ -49,17 +50,7 @@ async fn test_forward_native_tokens() {
             / rounder
     );
 
-    let result = wrap_owner
-        .call(&forwarder, "forward")
-        .args_json(json!({
-            "token_id": "near"
-        }))
-        .deposit(NearToken::from_yoctonear(1))
-        .max_gas()
-        .transact()
-        .await
-        .unwrap();
-    assert!(result.is_success());
+    factory.forward(&forwarder, &NEAR).await.unwrap();
 
     let fwd_balance = sandbox.balance(&forwarder).await;
     assert_eq!(fwd_balance / rounder, 1800);
@@ -68,6 +59,64 @@ async fn test_forward_native_tokens() {
     let deposit = transfer.as_yoctonear() - fee;
 
     assert_eq!(wrap.ft_balance_of(fees.id()).await / rounder, fee / rounder);
+    assert_eq!(
+        wrap.ft_balance_of(silo.id()).await / rounder,
+        deposit / rounder
+    );
+    assert_eq!(
+        erc20.balance_of(super::RECEIVER).await / rounder,
+        deposit / rounder
+    );
+}
+
+#[tokio::test]
+async fn test_forward_native_tokens_with_zero_fee() {
+    let transfer = NearToken::from_near(1);
+    let sandbox = Sandbox::new().await.unwrap();
+    let alice = sandbox.create_subaccount("alice", BALANCE).await.unwrap();
+    let (wrap, _) = sandbox.deploy_wrap_near().await.unwrap();
+    let fees = sandbox.deploy_fees(&[]).await.unwrap();
+    let silo = sandbox.deploy_aurora("silo").await.unwrap();
+    let erc20 = silo.deploy_erc20(wrap.id()).await.unwrap();
+    let factory = sandbox.deploy_factory(fees.id()).await.unwrap();
+
+    let forwarder = factory
+        .create(&[DeployParameters {
+            target_address: super::RECEIVER.to_string(),
+            target_network: silo.id().as_str().parse().unwrap(),
+            wnear_contract_id: wrap.id().as_str().parse().unwrap(),
+        }])
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    wrap.storage_deposit(alice.id()).await.unwrap();
+    wrap.storage_deposit(fees.id()).await.unwrap();
+    wrap.storage_deposit(silo.id()).await.unwrap();
+
+    let result = alice.transfer_near(&forwarder, transfer).await.unwrap();
+    assert!(result.is_success());
+
+    let rounder = 10u128.pow(21);
+    let fwd_balance = sandbox.balance(&forwarder).await;
+    assert_eq!(
+        fwd_balance / rounder,
+        transfer
+            .checked_add(NearToken::from_millinear(1800))
+            .unwrap()
+            .as_yoctonear()
+            / rounder
+    );
+
+    factory.forward(&forwarder, &NEAR).await.unwrap();
+
+    let fwd_balance = sandbox.balance(&forwarder).await;
+    assert_eq!(fwd_balance / rounder, 1800);
+
+    let deposit = transfer.as_yoctonear();
+
+    assert_eq!(wrap.ft_balance_of(fees.id()).await, 0);
     assert_eq!(
         wrap.ft_balance_of(silo.id()).await / rounder,
         deposit / rounder
